2ffa550f1fb71db1cc120c3a555dfda90132092a
Nurmukhanbet <nurmukhanbetsertay@gmail.com>
2024-09-16 14:15:18 +0500

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Nurmukhanbet Sertay on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
